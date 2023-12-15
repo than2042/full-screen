137a5b79ab3119ccfa54f68bcdecfa0070da20d4
than2042 <than.than2042@gmail.com>
2023-12-15 18:47:43 +0000

    add full image function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let currentImg = 0;
+let data = [];
 
 async function getImages(query) {
   const response = await fetch(
@@ -6,60 +7,128 @@ async function getImages(query) {
   );
 
   const json = await response.json();
-  displayImages(json.results);
+  data = json.results;
+  displayImages();
 }
 
-// create image
-const displayImages = (data) => {
+const displayImages = () => {
   const thumbnail = document.getElementById("thumbnail");
-  //   const displayEach = document.getElementById("display");
-  //   const carousel = document.querySelector(".carousel");
-
   thumbnail.innerHTML = "";
 
-  data.map((imgItem) => {
+  data.forEach((imgItem, index) => {
     const image = document.createElement("img");
-    image.src = imgItem.urls.full;
+    image.src = imgItem.urls.small;
     image.alt = imgItem.alt_description;
-    image.classList.add(".thumbnail");
+    image.classList.add("thumbnail");
+    image.tabIndex = 0;
+
+    thumbnail.setAttribute("role", "status");
+    thumbnail.setAttribute("aria-live", "assertive");
+    thumbnail.setAttribute("aria-atomic", "true");
+
+    // Add a click event listener to each thumbnail
+    image.addEventListener("click", function () {
+      showImage(index);
+    });
 
     thumbnail.appendChild(image);
-
-    console.log(imgItem);
   });
 
-  slider(currentImg);
+  // Initial display of the first image
+  showImage(currentImg);
 };
 
-const slider = (index) => {
-  //   const carousel = document.querySelector(".carousel");
-  const slides = document.querySelectorAll(".thumbnail");
-  const totalSildes = slides.length;
+const showImage = (index) => {
+  //   const slides = document.querySelectorAll(".thumbnail");
+  const displayImage = document.getElementById("display");
 
-  if (index < 0) {
-    currentImg = totalSildes - 1;
-  } else if (index >= totalSildes) {
-    currentImg = 0;
-  } else {
-    currentImg = index;
+  const newIndex = index <= 0 ? 0 : index;
+
+  currentImg = newIndex;
+
+  // Clear the display container
+  displayImage.innerHTML = "";
+
+  const image = document.createElement("img");
+  const p = document.createElement("p");
+  image.src = data[index].urls.full;
+  image.alt = data[index].alt_description;
+  p.textContent = data[index].alt_description;
+  image.classList.add("display");
+  p.classList.add("description");
+
+  displayImage.setAttribute("role", "status");
+  displayImage.setAttribute("aria-live", "assertive");
+  displayImage.setAttribute("aria-atomic", "true");
+
+  displayImage.appendChild(image);
+  displayImage.appendChild(p);
+};
+// showImage(currentImg);
+
+// const slider = document.querySelector(".thumbContainer");
+// const sildes = document.querySelectorAll(".item");
+const prevButton = document.getElementById("prevBtn");
+const nextButton = document.getElementById("nextBtn");
+prevButton.addEventListener("click", function () {
+  if (currentImg > 0) {
+    showImage(currentImg - 1);
   }
-};
-
-document.getElementById("prevBtn").addEventListener("click", function () {
-  slider(currentImg - 1);
 });
 
-document.getElementById("nextBtn").addEventListener("click", function () {
-  slider(currentImg + 1);
+nextButton.addEventListener("click", function () {
+  if (currentImg < data.length - 1) {
+    showImage(currentImg + 1);
+  } else {
+    showImage(0);
+  }
 });
 
-const form = document.getElementById("searchImg");
-
-form.addEventListener("submit", function (e) {
+const searchImage = document.getElementById("searchImg");
+searchImage.addEventListener("submit", function (e) {
   e.preventDefault();
-
   const queryImg = e.target.query.value;
   getImages(queryImg);
 });
 
-getImages("london");
+// keyboard events
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft" && currentImg > 0) {
+    showImage(currentImg - 1);
+  } else if (e.key === "ArrowRight" && currentImg < data.length - 1) {
+    showImage(currentImg + 1);
+  }
+});
+
+const skipContent = () => {
+  const mainContent = document.getElementById("galleryContainter");
+  mainContent.focus();
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Tab") {
+    const skipBtn = document.getElementById("skipContent");
+    skipBtn.style.display = "flex";
+    skipBtn.style.left = "auto";
+    skipBtn.focus();
+  }
+});
+
+document.getElementById("thumbnail").addEventListener("keydown", function (e) {
+  const thumbnails = document.querySelectorAll(".thumbnail");
+
+  if (e.key === "ArrowLeft") {
+    const newIndex = currentImg > 0 ? currentImg - 1 : 0;
+    thumbnails[newIndex].focus();
+  } else if (e.key === "ArrowRight") {
+    const newIndex =
+      currentImg < thumbnails.length - 1
+        ? currentImg + 1
+        : thumbnails.length - 1;
+    thumbnails[newIndex].focus();
+  }
+});
+
+// Initial fetch
+getImages("modern-art");
